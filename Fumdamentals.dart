4fcb68e2d2108c63a270f2585f4eb919c85cdab3
RahimAhmedKhan <155481138+roronoazoro25656@users.noreply.github.com>
2024-12-13 09:47:41 -0800

    Add files via upload

---
+++ Fumdamentals.dart
@@ -0,0 +1,55 @@
+void main() {
+  // Questuion 1
+  print("TASK # 1");
+
+  int Age = 21;
+  double productPrice = 499.9;
+  String Name = " Rahim Ahmed Khan  ";
+  bool Holiday = false;
+
+  print("My age is $Age");
+  print("This Football Price is $productPrice rupee");
+  print("My Self $Name");
+  print("is Today is a Holiday ? $Holiday");
+
+  print(
+      "================================================================================================");
+  //Question 2
+  print("TASK # 2");
+  int a = 56;
+  int b = 44;
+
+  print("A D D I T I O N of $a  and  $b  is  ${a + b} ");
+  print("S U B T R A C T I O N of $a  and  $b  is  ${a - b} ");
+  print("M U L T I P L I C T I O N of $a  and  $b  is  ${a * b} ");
+  print("D I V I O N of $a  and  $b  is  ${a / b} ");
+  print("M O D U L U S of $a  and  $b  is  ${a % b} ");
+
+  print(
+      "================================================================================================");
+  //Question 3
+  print("TASK # 3");
+  double x = 39.555;
+  double y = 101.34;
+
+  print(" is $x is graeter Than $y = ${x > y}");
+  print(" is $x is Less Than $y = ${x < y}");
+  print(" is $x is Equal to $y = ${x == y}");
+  print(" is $x is Not Equal to $y = ${x != y}");
+
+  print(
+      "================================================================================================");
+  //Question 4
+  print("TASK # 4");
+
+  int Num1 = 65;
+  double Num2 = 65.097;
+
+  var Sum = Num1 + Num2;
+  var Multi = Num1 * Num2;
+
+  print(
+      "Addtion of $Num1 and $Num2 is = $Sum and RuntimeType is ${Sum.runtimeType}");
+  print(
+      "Multiplication of $Num1 and $Num2 is = $Multi and RuntimeType is ${Sum.runtimeType}");
+}
